c5aa3f81b8f38ae720b7662d90ffaaad51ff3cff
David Kaufmann <astra@ionic.at>
2023-03-07 23:32:40 +0100

    cleanup, do smtp directly

--- newsletter.py
+++ newsletter.py
@@ -26,41 +26,45 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import argparse, subprocess, sys, time
+import argparse
+# import subprocess
+import sys
+import time
+import smtplib
 from email.message import EmailMessage
-from email.policy import SMTP
+from email.policy import SMTPUTF8
 
 parser = argparse.ArgumentParser(
         description='Newsletter sending script.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-s', '--subject', default='Newsletter der Fachschaft Informatik',
-        help="Subject of the mail")
+                    help="Subject of the mail")
 parser.add_argument('-f', '--from', dest='frm', default='Fachschaft Informatik <fsinf@fsinf.at>',
-        help="From header of the mail")
+                    help="From header of the mail")
 parser.add_argument('-t', '--to', default='noreply@fsinf.at',
-        help="To header of the mail. The addresses will receive the mail mulitiple times if the number of recipients is larger than --count.")
+                    help="To header of the mail. The addresses will receive the mail mulitiple times if the number of recipients is larger than --count.")
 parser.add_argument('--recipients', default="recipients.txt", metavar="FILE",
-        help="A file that lists all the recipients, one on each line")
+                    help="A file that lists all the recipients, one on each line")
 parser.add_argument('--blacklist', default="blacklist.txt", metavar="FILE",
-        help="A file that lists all the recipients that no longer want to receive this newsletter - same format as the \"recipients\" parameter")
+                    help="A file that lists all the recipients that no longer want to receive this newsletter - same format as the \"recipients\" parameter")
 parser.add_argument('--newsletter', default="newsletter.txt", metavar="FILE",
-        help="A file containing the text for this newsletter")
+                    help="A file containing the text for this newsletter")
 parser.add_argument('--header', default="header.txt", metavar="FILE",
-        help="A file containing a header prefixed to every newsletter")
+                    help="A file containing a header prefixed to every newsletter")
 parser.add_argument('--no-header', action='store_true', default=False,
-        help="Do not prefix this newsletter with a header")
+                    help="Do not prefix this newsletter with a header")
 parser.add_argument('--footer', default="footer.txt", metavar="FILE",
-        help="A file containing a footer suffixed to every newsletter")
+                    help="A file containing a footer suffixed to every newsletter")
 parser.add_argument('--no-footer', action='store_true', default=False,
-        help="Do not suffix this newsletter with a footer")
+                    help="Do not suffix this newsletter with a footer")
 parser.add_argument('--print-mail', action='store_true', default=False,
-        help="Print the mail as it would be send and exit.")
+                    help="Print the mail as it would be send and exit.")
 parser.add_argument('--count', default=100, type=int, metavar='N',
-        help="Send to N recipients at once")
+                    help="Send to N recipients at once")
 parser.add_argument('--sleep', default=120, type=int, metavar='SECS',
-        help="Sleep SECS seconds before sending the next mail")
+                    help="Sleep SECS seconds before sending the next mail")
 parser.add_argument('--dry-run', default=False, action="store_true",
-        help="Don't really send mail, only act like it")
+                    help="Don't really send mail, only act like it")
 args = parser.parse_args()
 
 # read files
@@ -74,7 +78,7 @@ if not args.no_footer:
     footer = open(args.footer, 'r').read()
 
 # remove email addresses contained in blacklist
-recipients = [ x for x in recipients if x not in blacklist ]
+recipients = [x for x in recipients if x not in blacklist]
 
 offset = 0
 while offset < len(recipients):
@@ -82,7 +86,7 @@ while offset < len(recipients):
     print('Mails to send: {}\u001b[0K'.format(len(recipients)-offset), end="\r")
     sys.stdout.flush()
 
-    mail = EmailMessage()
+    mail = EmailMessage(policy=SMTPUTF8)
 
     mail.add_header("From", args.frm)
 
@@ -93,7 +97,7 @@ while offset < len(recipients):
     mail.add_header("MIME-Version", "1.0")
 
     # quoted-printable is still more common, but we have upstream autoconvert, so stick to utf8
-    mail.set_content("{}{}{}".format(header, newsletter, footer), cte="8bit")
+    mail.set_content("{}{}{}".format(header, newsletter, footer), charset="utf-8", cte="8bit")
 
     # add recipients to bcc
     bcc_list = recipients[offset:][:args.count]
@@ -103,14 +107,12 @@ while offset < len(recipients):
         print(mail)
 
     if not args.dry_run:
-        sendmail = [ '/usr/sbin/sendmail', '-oi', '-t' ]
-        p = subprocess.Popen(sendmail, stdin=subprocess.PIPE)
-        # policy is required that escaping is done properly
-        stdout, stderr = p.communicate(mail.as_string(policy=SMTP).encode('utf-8'))
+        smtp = smtplib.SMTP("localhost")
+        smtp.send_message(mail)
 
     # increment counter
     offset += args.count
     time.sleep(args.sleep)
 
 # print "Done" and clear line
-print ("Done.\u001b[0K")
+print("Done.\u001b[0K")
